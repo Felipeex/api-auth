2269b6292d643e34be38f4859cf80370db5cdd0c
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2022-06-08 17:14:11 -0300

    Verificar sé conta existe em toLowerCase.

--- src/controllers/authControllers/signup.js
+++ src/controllers/authControllers/signup.js
@@ -24,7 +24,7 @@ async function AuthVerifyAccontExists(req, res, next) {
     const { email } = req.body
 
     try {
-        const usersByEmail = await users.find({ email: email})
+        const usersByEmail = await users.find({ email: email.toLowerCase() })
         if(usersByEmail.length > 0)
         return Bad(res, "Conta Existente")
 
